209eb7aa1f3adc7358111df61c44b1bebf6f6605
zengzheng <zengzheng2009@gmail.com>
2017-07-04 16:52:58 +0800

    rails new

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/jquery-1" => "pages#jquery_1"
+  get "/jquery-2" => "pages#jquery_2"
+  get "/jquery-3" => "pages#jquery_3"
+  get "/jquery-4" => "pages#jquery_4"
+  get "/jquery-5" => "pages#jquery_5"
+
+  root "pages#jquery_1"
+
+
+end
